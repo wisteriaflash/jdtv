2149f3b44f832950e90ba379cb601ef90be7faf1
wisteriaflash <wisteriaflash@163.com>
2014-05-30 19:07:52 +0800

    添加弹框 更新icon

--- 20140529-jd-oa/page/js/app.js
+++ 20140529-jd-oa/page/js/app.js
@@ -45,7 +45,48 @@ $(function(){
         hideText: 'Show less'
     });
     //popup
-    
+    $(".oa-icon-mod").click(function(e){
+        e.preventDefault();
+        //center
+        var popNode = $('#J_popup');
+        var _scrollHeight = $(document).scrollTop(),//获取当前窗口距离页面顶部高度
+        _windowHeight = $(window).height(),//获取当前窗口高度
+        _windowWidth = $(window).width(),//获取当前窗口宽度
+        _popupHeight = popNode.height(),//获取弹出层高度
+        _popupWeight = popNode.width();//获取弹出层宽度
+        _posiTop = (_windowHeight - _popupHeight)/2 + _scrollHeight;
+        _posiLeft = (_windowWidth - _popupWeight)/2;
+        // popNode
+        popNode.css({
+            left: _posiLeft,
+            top: _posiTop
+        });
+        var cls = $(this).attr('class').match(/pop\d/);
+        cls = cls ? cls : 'pop1';
+        popNode.find('.popup-item').hide();
+        popNode.find('.popup-item').filter('.'+cls).show();
+        popNode.show();
+    });
+    $('#J_popup .tab-hd li').click(function(e){
+        if($(this).hasClass('cur')){
+            return
+        }
+        var index = $(this).index();
+        var parent = $(this).parents('.popup-item');
+        parent.find('.tab-hd li').removeClass('cur');
+        $(this).addClass('cur');
+        //bd
+        var bdNode = parent.find('.tab-bd');
+        bdNode.removeClass('cur');
+        bdNode.eq(index).addClass('cur');
+    });
+    $('#J_popup .close').click(function(e){
+        $('#J_popup').hide();
+    });
+    $('#J_popup .btn-cancel').click(function(e){
+        e.preventDefault();
+        $('#J_popup').hide();
+    });
 });
 
 /*
